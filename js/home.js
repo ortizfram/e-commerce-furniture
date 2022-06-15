a7ad9b8e8ee5db079ad2a6fe8e41d8737dfc5683
melon <ortizfranco48@gmail.com>
2022-06-14 20:46:00 -0800

    adding blur to collage

--- js/home.js
+++ js/home.js
@@ -1,9 +1,24 @@
+//navbar when scroll white 
 const navbar = document.querySelector('.navbar');
 
 window.addEventListener('scroll', () => {
-        if (scrollY >= 250) {
-            navbar.classList.add('bg');
-        } else {
-            navbar.classList.remove('bg');
-        }
+    if (scrollY >= 250) {
+        navbar.classList.add('bg');
+    } else {
+        navbar.classList.remove('bg');
+    }
+})
+
+//image collage blur 
+const collageImages = [...document.querySelectorAll('.collage-img')]
+
+collageImages.map((item, i) => {
+    item.addEventListener('mouseover', () => {
+        collageImages.map((image, index) => {
+            if (index != i) {
+                image.style.filter = `blur(10px)`;
+                item.style.zIndex = 2;
+            }
+        })
     })
+})
